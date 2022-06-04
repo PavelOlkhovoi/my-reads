f57fed89d79da607256f7b3cd6185162d950f3e9
Pavel <p-olhovoi@bk.ru>
2022-06-04 11:23:47 +0200

    feat: Add the component ChangeCategory

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,8 +1,23 @@
-// import Categories from "./Categories";
-
+import ChangeCategory from "./ChangeCategory ";
 const Book = () => {
     return (
-        <h3>This book</h3>
+        <div className="book">
+        <div className="book-top">
+          <div
+            className="book-cover"
+            style={{
+              width: 128,
+              height: 188,
+              backgroundImage:
+                'url("http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api")',
+            }}
+          ></div>
+        <ChangeCategory />
+
+        </div>
+        <div className="book-title">Ender's Game</div>
+        <div className="book-authors">Orson Scott Card</div>
+      </div>
     )
 }
 
