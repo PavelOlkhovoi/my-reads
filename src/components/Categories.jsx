8e0873ffaabc15bb866f1ce6b9addc38dcae251f
Pavel <p-olhovoi@bk.ru>
2022-06-04 15:13:56 +0200

    feat: Add the Category component

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,37 +1,12 @@
 import Book from "./Book";
+import Category from "./Category";
 
 const Categories = () => {
     return (
     <div className="list-books-content">
-        <div className="bookshelf">
-            <h2 className="bookshelf-title">Currently Reading</h2>
-            <div className="bookshelf-books">
-                <ol className="books-grid">
-                    <li>
-                        <Book />
-                    </li>
-                    <li>
-                        <Book />
-                    </li>
-                </ol>
-            </div>
-        </div>
-
-        <div className="bookshelf">
-            <h2 className="bookshelf-title">Read</h2>
-            <div className="bookshelf-books">
-                <ol className="books-grid">
-                    <li>
-                        <Book />
-                    </li>
-                    <li>
-                        <Book />
-                    </li>
-                </ol>
-            </div>
-        </div>
-
-
+        <Category />
+        <Category />
+        <Category />
     </div>
     )
 }
