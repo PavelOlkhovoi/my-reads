bbcf4b77b3ef22674460307dbbc05af4d92d1ef1
Pavel <p-olhovoi@bk.ru>
2022-06-10 11:52:08 +0200

    feat: Get all books from API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import { useState, useEffect, useCallback } from "react";
+import {getAll} from "./BooksAPI"
 import "./App.css";
-// import { useState } from "react";
 import Home from "./components/Home.jsx";
 
 
 function App() {
+  const [books, setBooks] = useState([]);
+  
+  const getAllBooks = useCallback(() => {
+    return getAll().then( allBooks => setBooks( books => [...books, allBooks]) )
+  }, []);
+
+  useEffect(()=> {
+    getAllBooks();
+    console.log(books)
+  }, [getAllBooks])
+  
   return (
     <div>
         <Home>
