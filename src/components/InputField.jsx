c8e1ddfdce99c6437daeb97bf6dee16a4b59bd65
Pavel <p-olhovoi@bk.ru>
2022-06-12 23:04:48 +0200

    feat: Add function searching books

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -1,9 +1,10 @@
-const InputField = ()=> {
+const InputField = ({searchBooks})=> {
     return (
         <div className="search-books-input-wrapper">
         <input
           type="text"
           placeholder="Search by title, author, or ISBN"
+          onChange={searchBooks}
         />
       </div>
     )
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,7 +1,21 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import InputField from "./InputField";
+import { search } from "../BooksAPI"
+import Book from "./Book";
 
 const Search = () => {
+    const [foundBooks, setFoundBooks] = useState([]);
+    // 1. Query  2. How mach books maximum can be added?
+    const searchBooks = async (e) => {
+        let words = e.target.value;
+        console.log(words)
+        const getBooks = await search(words, 10);
+        console.log(getBooks)
+        setFoundBooks(getBooks)
+        console.log(foundBooks);
+        }
+
     return (
     <div className="search-books">
         <div className="search-books-bar">
@@ -10,10 +24,14 @@ const Search = () => {
         >
             Close
         </Link>
-        <InputField/>
+        <InputField searchBooks={searchBooks}/>
         </div>
         <div className="search-books-results">
-        <ol className="books-grid"></ol>
+        <ol className="books-grid">
+            {
+                !foundBooks.error ? foundBooks.map( book => <Book book={book}></Book>) : ""
+            }
+        </ol>
         </div>
     </div>
     )
